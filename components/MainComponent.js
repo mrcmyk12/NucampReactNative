ec59fe33d12f84aab0337abdf0b14e2a4b03d9aa
Philip Sanders <mrcmyk12@gmail.com>
2021-05-17 19:04:46 -0700

    Icons, Favorites, Comments

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,77 +1,113 @@
-import React, {Component} from 'react';
-import Directory from './DirectoryComponent';
-import Home from './HomeComponent';
-import CampsiteInfo from './CampsiteInfoComponent';
-import {View, Platform} from 'react-native';
-import { createStackNavigator } from 'react-navigation-stack';
-import {createAppContainer} from 'react-navigation';
-import {createDrawNavigator} from 'react-navigation';
-import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
-import { createDrawerNavigator } from 'react-navigation-drawer';
+import React, { Component } from "react";
+import Directory from "./DirectoryComponent";
+import Home from "./HomeComponent";
+import About from "./AboutComponent";
+import Contact from "./ContactComponent";
+import CampsiteInfo from "./CampsiteInfoComponent";
+import { View, Platform } from "react-native";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import { createDrawNavigator } from "react-navigation";
+import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
+import { createDrawerNavigator } from "react-navigation-drawer";
 
 const DirectoryNavigator = createStackNavigator(
-    {
-        Directory:{ screen: Directory },
-        CampsiteInfo: {screen: CampsiteInfo }
-    },
-    {
-        initialRouteName: 'Directory',
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: '#5637DD'
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-                color: '#fff'
-            }
-        }
-    }
+	{
+		Directory: { screen: Directory },
+		CampsiteInfo: { screen: CampsiteInfo },
+	},
+	{
+		initialRouteName: "Directory",
+		defaultNavigationOptions: {
+			headerStyle: {
+				backgroundColor: "#5637DD",
+			},
+			headerTintColor: "#fff",
+			headerTitleStyle: {
+				color: "#fff",
+			},
+		},
+	}
 );
 
 const HomeNavigtor = createStackNavigator(
-    {
-        Directory:{ screen: Home },
-    },
-    {
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: '#5637DD'
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-                color: '#fff'
-            }
-        }
-    }
+	{
+		Home: { screen: Home },
+	},
+	{
+		defaultNavigationOptions: {
+			headerStyle: {
+				backgroundColor: "#5637DD",
+			},
+			headerTintColor: "#fff",
+			headerTitleStyle: {
+				color: "#fff",
+			},
+		},
+	}
+);
 
+const AboutNavigtor = createStackNavigator(
+	{
+		About: { screen: About },
+	},
+	{
+		defaultNavigationOptions: {
+			headerStyle: {
+				backgroundColor: "#5637DD",
+			},
+			headerTintColor: "#fff",
+			headerTitleStyle: {
+				color: "#fff",
+			},
+		},
+	}
+);
+
+const ContactNavigator = createStackNavigator(
+	{
+		Contact: { screen: Contact },
+	},
+	{
+		defaultNavigationOptions: {
+			headerStyle: {
+				backgroundColor: "#5637DD",
+			},
+			headerTintColor: "#fff",
+			headerTitleStyle: {
+				color: "#fff",
+			},
+		},
+	}
 );
 
 const MainNavigator = createDrawerNavigator(
-    {
-        Home: { screen: HomeNavigtor },
-        Directory: { screen: DirectoryNavigator }
-    },
-    {
-        drawerBackgroundColor: '#cec8ff'
-    }
-)
+	{
+		Home: { screen: HomeNavigtor },
+		Directory: { screen: DirectoryNavigator },
+		About: { screen: AboutNavigtor },
+		Contact: { screen: ContactNavigator },
+	},
+	{
+		drawerBackgroundColor: "#cec8ff",
+	}
+);
 
 const AppNavigator = createAppContainer(MainNavigator);
 
-
 class Main extends Component {
-
-    render(){
-        return (
-            <View 
-                style={{
-                    flex:1,
-                    paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
-                    }}>
-                <AppNavigator />
-            </View>
-        );
-    }
+	render() {
+		return (
+			<View
+				style={{
+					flex: 1,
+					paddingTop:
+						Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight,
+				}}>
+				<AppNavigator />
+			</View>
+		);
+	}
 }
 
-export default Main; 
+export default Main;
